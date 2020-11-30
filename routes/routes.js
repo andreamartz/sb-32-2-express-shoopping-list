9a0d836ac89d055551834b772e6f16479ee1d359
andreamartz <14165788+andreamartz@users.noreply.github.com>
2020-11-30 09:30:44 -0600

    Improve routes with try/catch blocks

--- routes/routes.js
+++ routes/routes.js
@@ -17,35 +17,47 @@ router.post('/', function(req, res, next) {
     items.push(newItem);
     return res.status(201).json({ added: newItem });
   } catch (e) {
-    return next(e)
+    return next(e);
   }
 });
 
 router.get('/:name', function(req, res, next) {
-  const foundItem = items.find(item => item.name === req.params.name);
-  if (foundItem === undefined) {
-    throw new ExpressError("Item not found", 404);
+  try {
+    const foundItem = items.find(item => item.name === req.params.name);
+    if (foundItem === undefined) {
+      throw new ExpressError("Item not found", 404);
+    }
+    return res.json({ item: foundItem });
+  } catch(e) {
+    return next(e);
   }
-  res.json({ item: foundItem });
 });
 
 router.patch('/:name', function(req, res, next) {
-  const foundItem = items.find(item => item.name === req.params.name);
-  if (foundItem === undefined) {
-    throw new ExpressError("Item not found", 404);
+  try {
+    const foundItem = items.find(item => item.name === req.params.name);
+    if (foundItem === undefined) {
+      throw new ExpressError("Item not found", 404);
+    }
+    foundItem.name = req.body.name || foundItem.name;
+    foundItem.price = req.body.price || foundItem.price;
+    return res.json({ updated: foundItem });
+  } catch (e) {
+    return next(e);
   }
-  foundItem.name = req.body.name || foundItem.name;
-  foundItem.price = req.body.price || foundItem.price;
-  res.json({ updated: foundItem });
 });
 
 router.delete('/:name', function(req, res, next) {
-  const foundItem = items.findIndex(item => item.name === req.params.name)
-  if (foundItem === -1) {
-    throw new ExpressError("Item not found", 404)
+  try {
+    const foundItem = items.findIndex(item => item.name === req.params.name);
+    if (foundItem === -1) {
+      throw new ExpressError("Item not found", 404);
+    }
+    items.splice(foundItem, 1);
+    return res.json({ message: "Deleted" });
+  } catch (e) {
+    next(e);
   }
-  items.splice(foundItem, 1)
-  res.json({ message: "Deleted" })
 });
 
 module.exports = router;
